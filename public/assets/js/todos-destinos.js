80af28d124721e05175feebc019eafc95f7268fd
rafaelbenetrip <rafael@benetrip.com.br>
2026-02-20 18:26:21 -0300

    Update todos-destinos.js

--- public/assets/js/todos-destinos.js
+++ public/assets/js/todos-destinos.js
@@ -17,7 +17,7 @@ const BenetripTodosDestinos = {
 
     config: {
         debug: true,
-        cidadesJsonPath: 'data/cidades_global_iata_v5.json'
+        cidadesJsonPath: 'data/cidades_global_iata_v7.json'
     },
 
     log(...args) {
